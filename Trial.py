04a0837cc803d81fa2c6522e8ea639262a19f0c8
Narendranath D Nadig <35164475+narennadig@users.noreply.github.com>
2019-01-30 19:59:29 +0530

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -9,19 +9,15 @@ for row in reader:
     i+=1
 print('The DMA table is:')
 
-for i in range(1,84):
+for i in range(1,85):
     print i,dic[i]
-
-
-
-
-
 
 count=0
 N = input('What DMA do you need?  ')
 t=N
 tool=str(dic[t])+'.csv'
 f = open(tool,'w')
+f.write('Month'+','+'Consumption'+'\n')
 N=N-1
 for j in range(1,16):
     trial=str(j)+'.csv'
@@ -34,7 +30,7 @@ for j in range(1,16):
         if i == N:
             print("This is the line.")
             print(row)
-            f.write(row[0])
+            f.write(str(j)+','+row[0])
             f.write('\n')
             count+=1
             break
@@ -43,5 +39,3 @@ for j in range(1,16):
         i += 1
 the_file.close()
 f.close()
-
-
